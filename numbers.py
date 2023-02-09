894b4bb03b8bed8c9174db5f64571395713329bd
Scott Hadzik <scotthadzik@weber.edu>
2023-02-08 18:49:06 -0700

    random number

--- numbers.py
+++ numbers.py
@@ -1,1 +1,15 @@
+import random
+
 # Python Numbers
+aNumber = 10 # created datatype int
+aFloatNumber = 1.2
+# aNumber = float(aNumber)
+
+
+# print (aNumber + aFloatNumber)
+
+print(random.randrange(0,900))
+
+
+
+
